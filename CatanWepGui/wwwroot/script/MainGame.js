0964efb0e68d9ab2fb5191ff8ef529aa44c80a91
momoluck <momoluck@groepl.site>
2025-03-17 15:44:14 +0100

    aibraMOmoLacki

--- CatanWepGui/wwwroot/script/MainGame.js
+++ CatanWepGui/wwwroot/script/MainGame.js
@@ -1,6 +1,22 @@
 ﻿window.Change = window.Change || {
-    changeElement: function (id) {
-        var idk = document.getElementById(id);
-        idk.style.visibility = "hidden";
+    changeElement: function (classs) {
+        var idk = document.querySelectorAll(classs);
+        for (const node of idk) {
+            if(node.classList.contains("active")) {
+                node.style.visibility = "visible";
+            }
+        }
+    }
+}
+
+window.Place = window.Place || {
+    placeBuilding: function (location,building) {
+        var element = document.getElementById(location);
+        element.classList.remove("active");
+        element.classList.add(building.id);
+        var idk = document.querySelectorAll(".intersec")
+        for (const node of idk) {
+            node.style.visibility = "hidden";
+        }
     }
 }

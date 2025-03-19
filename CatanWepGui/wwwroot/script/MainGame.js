107657ed50a6b25fb08738da5bc8ff3c237c3290
momoluck <momoluck@groepl.site>
2025-03-19 18:51:19 +0100

    arrrrriba

--- CatanWepGui/wwwroot/script/MainGame.js
+++ CatanWepGui/wwwroot/script/MainGame.js
@@ -14,9 +14,14 @@ window.Place = window.Place || {
         element.classList.add(building.id);
         element.style.height = "20px";
         element.style.width = "20px";
-        element.style.top = (element.style.top.toString()[0] + element.style.height.toString()[1]) - (-10) +"px";
-        element.style.left = (element.style.left.toString()[0] + element.style.left.toString()[1]) - (-10) +"px";
+        
+        element.style.top = (element.style.top).toString().split("px")[0] - 5 +"px";
+        element.style.left = (element.style.left).toString().split("px")[0] - 5 +"px";
+        elementPlus = document.querySelectorAll(".intersec")[element.id.split("_")[2] -1 + 1]
+        document.querySelectorAll(".intersec")[element.id.split("_")[2] -1 + 1].style.left = 
+        
         element.style.backgroundColor = color;
+        console.log("asdf")
         var idk = document.querySelectorAll(".intersec")
         for (const node of idk) {
             if(node.classList.contains("active")) {

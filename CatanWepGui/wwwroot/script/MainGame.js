a5faf19a046b302a8ad1febdd1d7484a94122f59
Moritz <M.boehmwalder@htlkrems.at>
2025-03-19 22:45:06 +0100

    asdffasdfasdfasdf

--- CatanWepGui/wwwroot/script/MainGame.js
+++ CatanWepGui/wwwroot/script/MainGame.js
@@ -10,25 +10,29 @@
 window.Place = window.Place || {
     placeBuilding: function (location,building, color) {
         var element = document.getElementById(location);
-        element.classList.remove("active");
-        element.classList.add(building.id);
-        element.style.height = "20px";
-        element.style.width = "20px";
-        
-        element.style.top = (element.style.top).toString().split("px")[0] - 5 +"px";
-        element.style.left = (element.style.left).toString().split("px")[0] - 5 +"px";
-        elementPlus = document.querySelectorAll(".intersec")[element.id.split("_")[2] -1 + 1]
-        document.querySelectorAll(".intersec")[element.id.split("_")[2] -1 + 1].style.left = 
-        
+        if(element.classList.contains("active")) {
+            element.classList.remove("active");
+            element.classList.add(building.id);
+            element.style.height = "20px";
+            element.style.width = "20px";
+
+            element.style.top = (element.style.top).toString().split("px")[0] - (0) + "px";
+            element.style.left = (element.style.left).toString().split("px")[0] - 5 + "px";
+            for (let i = element.id.split("_")[2] - 1 + 1; i < document.querySelectorAll(".intersec").length; i++) {
+                elementPlus = document.querySelectorAll(".intersec")[i]
+                elementPlus.style.left = (elementPlus.style.left).toString().split("px")[0] - 10 + "px";
+            }
+        }
         element.style.backgroundColor = color;
-        console.log("asdf")
         var idk = document.querySelectorAll(".intersec")
         for (const node of idk) {
             if(node.classList.contains("active")) {
-                node.style.visibility = "hidden";
-            } else {
-                node.style.visibility = "visible";
+                node.backgroundColor = "rgb(255,255,255)";
+                node.style.visibility = "hidden"; //hidden
+                node.backgroundColor = "rgb(0, 160, 255)";
+
             }
         }
+
     }
 }

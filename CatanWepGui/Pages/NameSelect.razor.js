2cf7b008d6f0b7173a3d76dcd77305292481ed7e
rudi <r.hubaczek@htlkrems.at>
2025-05-05 08:47:40 +0200

    fhadsjkcseölurhozpngbu

--- CatanWepGui/Pages/NameSelect.razor.js
+++ CatanWepGui/Pages/NameSelect.razor.js
@@ -1,3 +1,12 @@
 window.getElementClass = (element) => {
     return element.className;
 };
+
+const colorInput = document.getElementById('colorInput');
+const selectedColorDiv = document.getElementById('selectedColor');
+
+colorInput.addEventListener('input', function() {
+    const selectedColor = colorInput.value;
+    selectedColorDiv.style.backgroundColor = selectedColor; // Hintergrund ändern
+    selectedColorDiv.textContent = `Ausgewählte Farbe: ${selectedColor}`; // Text ändern
+});
